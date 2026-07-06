bcb5f0a184dda7aacd9325ff53abb59d29517bd1
Aveeral <aveeraljain22@gmail.com>
2026-07-06 13:00:19 +0530

    Done with auth routes

---
+++ capstone/src/routes/auth.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const { validate } = require("../middleware/validate.js");
+const { registerSchema, loginSchema } = require("../schemas/authSchemas.js"); // you'll define these two Zod schemas
+const { registerController, loginController, refreshController, logoutController } = require("../controllers/authController.js");
+
+router.post("/register", validate(registerSchema), registerController);
+router.post("/login", validate(loginSchema), loginController);
+router.post("/refresh", refreshController);
+router.post("/logout", logoutController);
+
+module.exports = router;
