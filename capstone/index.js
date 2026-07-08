dd3f8c3f20724c7b96584dcaa850c50d2ca40def
Aveeral <aveeraljain22@gmail.com>
2026-07-08 20:57:23 +0530

    Fix error: change index.js path

--- capstone/index.js
+++ capstone/index.js
@@ -3,12 +3,12 @@ const session = require("express-session");
 const helmet = require("helmet");
 const cookieParser = require("cookie-parser");
 
-const { PORT, SESSION_SECRET, NODE_ENV } = require("./config/env.js");
-const { errorHandler } = require("./middleware/errorHandler.js");
+const { PORT, SESSION_SECRET, NODE_ENV } = require("./src/config/env.js");
+const { errorHandler } = require("./src/middleware/errorHandler.js");
 
-const authRoutes = require("./routes/auth.js");
-const keyRoutes = require("./routes/keys.js");
-const oauthRoutes = require("./routes/oauth.js");
+const authRoutes = require("./src/routes/auth.js");
+const keyRoutes = require("./src/routes/keys.js");
+const oauthRoutes = require("./src/routes/oauth.js");
 
 const app = express();
 
