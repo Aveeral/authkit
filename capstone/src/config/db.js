61c5e930733972ab025d6eeb28954e3748a4013a
Aveeral <aveeraljain22@gmail.com>
2026-07-15 12:13:54 +0530

    Added migrations folder

--- capstone/src/config/db.js
+++ capstone/src/config/db.js
@@ -1,9 +1,16 @@
-const {Pool} = require("pg");
-const {DATABASE_URL} = require("./env.js");
- 
- const pool = new Pool({
-     connectionString: DATABASE_URL,
-     ssl: {rejectUnauthorized: false}
- })
- 
- module.exports = pool
+const { Pool } = require("pg");
+const { DATABASE_URL, NODE_ENV } = require("./env.js");
+
+let sslConfig;
+if (NODE_ENV === "production") {
+  sslConfig = { rejectUnauthorized: true };
+} else {
+  sslConfig = { rejectUnauthorized: false };
+}
+
+const pool = new Pool({
+  connectionString: DATABASE_URL,
+  ssl: sslConfig
+});
+
+module.exports = pool;
